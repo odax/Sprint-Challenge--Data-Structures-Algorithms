173de05345d526ad8758d3fc2ae234d7612e7e7c
odax <nr10@albion.edu>
2018-06-08 14:05:20 -0400

    completed depthFirstForEach breadthFirstForEach

--- data_structures/src/binary-search-tree.js
+++ data_structures/src/binary-search-tree.js
@@ -5,15 +5,44 @@ class BinarySearchTree {
     this.right = null;
   }
 
+  /*depthFirstForEach(cb) receives a callback function as a parameter. This method iterates over the binary 
+  search tree in depth-first order, applying the supplied callback function to each tree element in turn.
+
+  breadthFirstForEach(cb) receives a callback function as a parameter. This method iterates over the binary 
+  search tree in breadth-first order, applying the supplied callback function to each tree element in turn.*
+  */
+
   depthFirstForEach(cb) {
-    /* Your code here */
-    
+    cb(this.value);
+    if (this.left) {
+      this.left.depthFirstForEach(cb);
+    }
+    if (this.right) {
+      the.right.depthFirstForEach(cb);
+    }
   }
 
-  breadthFirstForEach(cb) {
-    /* Your code here */
-
-  }
+breadthFirstForEach(cb) {
+      const nodes = [];
+      nodes.push(this.value);
+      let left = this.left;
+      let right = this.right;
+      function populator(left, right) {
+        if (left && right) nodes.push(left.value, right.value);
+        else if (left) nodes.push(left.value);
+        else if (right) nodes.push(right.value);
+   
+        if (left && right) {
+          populator(left.left, left.right);
+          populator(right.left, right.right);
+        } else if (left) populator(left.left, left.right);
+        else if (right) populator(right.left, right.right);
+        if (!left && !right) return;
+      }
+      populator(this.left, this.right);
+      nodes.forEach(num => cb(num));
+     }
+}
 
   insert(value) {
     const newNode = new BinarySearchTree(value);
